51b850048df5b5719a08e62dde4915e5969aa7f8
totetmatt <matthieu.totet@gmail.com>
2022-12-01 08:24:06 +0100

    day_01

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,17 +1,32 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-
+use std::cmp::Reverse;
 fn main() {
-    // File hosts must exist in current path before this produces output
-    if let Ok(lines) = read_lines("./inputs/day_01/test.txt") {
+    let mut elves=vec![0];
+    if let Ok(lines) = read_lines("./inputs/day_01/input") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
-                println!("{}", ip);
+            
+                match ip.parse::<i64>() {
+                    Ok(v) => 
+                        {
+                            let last = elves.last_mut().unwrap() ;
+                            *last += v
+                        },
+                    _ => elves.push(0),
+                }
             }
         }
     }
+    elves.sort_by_key(|w| Reverse(*w));
+    let max = elves.first().unwrap();
+    println!("{max:?}");
+    let top3 = elves[0..3].iter();
+    let maxes : i64 = top3.sum();
+    println!("{maxes}")
+
 }
 
 // The output is wrapped in a Result to allow matching on errors
