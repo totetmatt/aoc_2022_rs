c4c43c90c974613bdc060338c7951a1babe476ca
totetmatt <matthieu.totet@gmail.com>
2022-12-02 14:03:43 +0100

    Day_02

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,19 +1,18 @@
+use std::cmp::Reverse;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::cmp::Reverse;
 fn main() {
-    let mut elves=vec![0];
+    let mut elves = vec![0];
     if let Ok(lines) = read_lines("./inputs/day_01/input") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
                 match ip.parse::<i64>() {
-                    Ok(v) => 
-                        {
-                            let last = elves.last_mut().unwrap() ;
-                            *last += v
-                        },
+                    Ok(v) => {
+                        let last = elves.last_mut().unwrap();
+                        *last += v
+                    }
                     _ => elves.push(0),
                 }
             }
@@ -23,15 +22,16 @@ fn main() {
     let max = elves.first().unwrap();
     println!("{max:?}");
     let top3 = elves[0..3].iter();
-    let maxes : i64 = top3.sum();
+    let maxes: i64 = top3.sum();
     println!("{maxes}")
-
 }
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
---
+++ src/day_02.rs
@@ -0,0 +1,83 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+/*
+Rock Paper Scissor
+
+ABC
+XYZ --> 3
+
+ABC
+ZXY --> 0
+
+ABC
+YZX --> 6
+*/
+fn main() -> Result<(), std::io::Error> {
+    let abc = String::from("ABC");
+    let xyz = String::from("XYZ");
+    let input_file: &str = "./inputs/day_02/input";
+    {
+        // Part 1
+        let file = &File::open(input_file)?;
+        let reader = io::BufReader::new(file);
+        let res: i32 = reader
+            .lines()
+            .map(|line| match line {
+                Ok(l) => {
+                    let opponent = l.chars().nth(0).unwrap();
+                    let me = l.chars().nth(2).unwrap();
+                    let a: i32 = abc.find(opponent).unwrap() as i32;
+                    let x: i32 = xyz.find(me).unwrap() as i32;
+                    x + 1i32
+                        + match a - x {
+                            -1 | 2 => 6i32,
+                            0 => 3i32,
+                            _ => 0i32,
+                        }
+                }
+                Err(_) => 0i32,
+            })
+            .sum();
+        println!("part 1 : {res}");
+    }
+
+    {
+        // Part 2
+        let res = &String::from("ABCABCABC");
+        let file = &File::open(input_file)?;
+        let reader = io::BufReader::new(file);
+        let result: i32 = reader
+            .lines()
+            .map(|line| match line {
+                Ok(l) => {
+                    let opponent = l.chars().nth(0).unwrap();
+                    let result = l.chars().nth(2).unwrap();
+                    let (idx, score): (i32, i32) = match result {
+                        'X' => (-1, 0),
+                        'Y' => (0, 3),
+                        'Z' => (1, 6),
+                        _ => (0, 0),
+                    };
+
+                    res.find(
+                        res.chars()
+                            .nth(res
+                                .find(opponent)
+                                .unwrap() 
+                                + (3 + idx) as usize
+                            )
+                            .unwrap(),
+                    )
+                    .unwrap() as i32
+                        + score
+                        + 1
+                }
+                Err(_) => 0i32,
+            })
+            .sum();
+        println!("part 2 : {result}");
+    }
+
+    Ok(())
+}
